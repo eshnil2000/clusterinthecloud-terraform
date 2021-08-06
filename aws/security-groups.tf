41d1cee8b0ff2a64a104ff4272a7e10be4674da3
Nick <eshnil2000@gmail.com>
2021-08-05 21:19:48 -0700

    Update security-groups.tf

--- aws/security-groups.tf
+++ aws/security-groups.tf
@@ -7,7 +7,7 @@ resource "aws_security_group" "mgmt" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["73.48.134.89/32"]
   }
 
   ingress {
